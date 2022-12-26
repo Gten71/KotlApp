1fae12bc6cfe65968169a436b95ebf9a1c73cbff
Gten71 <mresnyansky@gmail.com>
2022-12-26 20:16:47 +0300

    edit reverse code

--- app/src/main/java/com/example/myapplication/ReverseActivity.kt
+++ app/src/main/java/com/example/myapplication/ReverseActivity.kt
@@ -3,31 +3,30 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class ReverseActivity : AppCompatActivity() {
+    private lateinit var text: EditText
+    private lateinit var result: TextView
+    private lateinit var rev: CheckBox
+    private lateinit var okButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reverse)
-        // Получаем ссылку на EditText
-        val editText = findViewById<EditText>(R.id.editTexts)
+        rev=findViewById(R.id.reverse_checkbox)
+        okButton=findViewById(R.id.Ok)
+        result=findViewById(R.id.result_text)
+        text=findViewById(R.id.editTexts)
 
-        // Получаем ссылку на TextView
-        val textView = findViewById<TextView>(R.id.result_text)
-
-        // Находим кнопку и устанавливаем слушатель события нажатия
-        val button = findViewById<Button>(R.id.Ok_button)
-        button.setOnClickListener {
-            // Получаем текст из EditText и разворачиваем его
-            val reversedText = editText.text.toString().reversed()
-
-            // Устанавливаем развернутый текст в TextView
-            textView.text = reversedText
+        okButton.setOnClickListener {
+            if (rev.isChecked) {
+                val text = text.text.toString()
+                val reversedText = text.reversed()
+                result.text = reversedText
+            }
         }
+
         val buttonBacks = findViewById<Button>(R.id.buttons_back)
         buttonBacks.setOnClickListener {
             finish()
