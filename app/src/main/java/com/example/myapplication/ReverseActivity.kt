d7684a82fa6b0270a2867b81d57f03e2dcc30299
Gten71 <mresnyansky@gmail.com>
2022-12-27 12:25:53 +0300

    add checkbox -> Uppercase and edit text save location for TextView

--- app/src/main/java/com/example/myapplication/ReverseActivity.kt
+++ app/src/main/java/com/example/myapplication/ReverseActivity.kt
@@ -11,6 +11,7 @@ class ReverseActivity : AppCompatActivity() {
     private lateinit var setText: EditText
     private lateinit var result: TextView
     private lateinit var rev: CheckBox
+    private lateinit var upper: CheckBox
     private lateinit var Clear: Button
     private lateinit var okButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,27 +20,35 @@ class ReverseActivity : AppCompatActivity() {
         rev=findViewById(R.id.reverse_checkbox)
         okButton=findViewById(R.id.Ok)
         result=findViewById(R.id.result_text)
+        upper=findViewById(R.id.uppercase_checkbox)
         setText=findViewById(R.id.editTexts)
         val sharedPref = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
         val text = sharedPref.getString("text", "")
         result.text = text
         okButton.setOnClickListener {
+            if (upper.isChecked){
+                val text = setText.text.toString()
+                val upperText = text.uppercase()
+                result.text=upperText
+            }
             if (rev.isChecked) {
                 val text = setText.text.toString()
                 val reversedText = text.reversed()
                 result.text = reversedText
 
             }
-            val text = result.text.toString()
-            val sharedPref = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-            val editor = sharedPref.edit()
-            editor.putString("text", text)
-            editor.apply()
+            if (rev.isChecked && upper.isChecked){
+                val text = setText.text.toString()
+                val allText = text.reversed().uppercase()
+                result.text=allText
+            }
+
 
         }
         Clear=findViewById(R.id.Clear_button)
         Clear.setOnClickListener {
             result.text = ""
+            upper.isChecked=false
             rev.isChecked=false
             setText.text.clear()
         }
@@ -47,7 +56,13 @@ class ReverseActivity : AppCompatActivity() {
 
         val buttonBacks = findViewById<Button>(R.id.buttons_back)
         buttonBacks.setOnClickListener {
+            val text = result.text.toString()
+            val sharedPref = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+            val editor = sharedPref.edit()
+            editor.putString("text", text)
+            editor.apply()
             finish()
+
         }
     }
 }
