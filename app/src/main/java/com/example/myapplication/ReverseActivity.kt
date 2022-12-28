7a69a6c446d7cb2f46ffa7bed41a33c36f91fb18
Gten71 <mresnyansky@gmail.com>
2022-12-28 22:20:21 +0300

    remade code

--- app/src/main/java/com/example/myapplication/ReverseActivity.kt
+++ app/src/main/java/com/example/myapplication/ReverseActivity.kt
@@ -12,11 +12,13 @@ class ReverseActivity : AppCompatActivity() {
     private lateinit var result: TextView
     private lateinit var rev: CheckBox
     private lateinit var upper: CheckBox
+    private lateinit var lover:CheckBox
     private lateinit var clear: Button
     private lateinit var okButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reverse)
+        lover=findViewById(R.id.lowercase_checkbox)
         rev=findViewById(R.id.reverse_checkbox)
         okButton=findViewById(R.id.Ok)
         result=findViewById(R.id.result_text)
@@ -24,27 +26,40 @@ class ReverseActivity : AppCompatActivity() {
         setText=findViewById(R.id.editTexts)
         val sharedPref = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
         val text = sharedPref.getString("text", "")
+
         result.text = text
+        fun lowercaseTramsform(text:String):String{
+            return text.lowercase()
+        }
+        fun uppercaseTransform(text: String): String {
+            return text.uppercase()
+        }
+        fun reverseTransform(text: String): String {
+            return text.reversed()
+        }
+
         okButton.setOnClickListener {
-            if (upper.isChecked){
-                val text = setText.text.toString()
-                val upperText = text.uppercase()
-                result.text=upperText
+            if (lover.isChecked){
+                val inputlower = setText.text.toString()
+                val resultlower=lowercaseTramsform(inputlower)
+                setText.setText(resultlower)
+                result.text=resultlower
             }
             if (rev.isChecked) {
-                val text = setText.text.toString()
-                val reversedText = text.reversed()
-                result.text = reversedText
-
+                val input = setText.text.toString()
+                val resulti = reverseTransform(input)
+                setText.setText(resulti)
+                result.text = resulti
             }
-            if (rev.isChecked && upper.isChecked){
-                val text = setText.text.toString()
-                val allText = text.reversed().uppercase()
-                result.text=allText
+            if (upper.isChecked){
+                val inputup = setText.text.toString()
+                val resultup = uppercaseTransform(inputup)
+                setText.setText(/* text = */ resultup)
+                result.text = resultup
             }
-
-
         }
+
+
         clear=findViewById(R.id.Clear_button)
         clear.setOnClickListener {
             result.text = ""
