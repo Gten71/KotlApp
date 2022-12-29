2c7c14aa3b3400bd535dbea1e5fc1fe98f466576
Gten71 <mresnyansky@gmail.com>
2022-12-29 10:18:41 +0300

    cannot select Uppercase and Lowercase together

--- app/src/main/java/com/example/myapplication/ReverseActivity.kt
+++ app/src/main/java/com/example/myapplication/ReverseActivity.kt
@@ -1,24 +1,23 @@
 package com.example.myapplication
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+
 
 class ReverseActivity : AppCompatActivity() {
     private lateinit var setText: EditText
     private lateinit var result: TextView
     private lateinit var rev: CheckBox
     private lateinit var upper: CheckBox
-    private lateinit var lover:CheckBox
+    private lateinit var lower:CheckBox
     private lateinit var clear: Button
     private lateinit var okButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reverse)
-        lover=findViewById(R.id.lowercase_checkbox)
+        lower=findViewById(R.id.lowercase_checkbox)
         rev=findViewById(R.id.reverse_checkbox)
         okButton=findViewById(R.id.Ok)
         result=findViewById(R.id.result_text)
@@ -28,20 +27,20 @@ class ReverseActivity : AppCompatActivity() {
         val text = sharedPref.getString("text", "")
 
         result.text = text
-        fun lowercaseTramsform(text:String):String{
+        fun lowercaseTransform(text:String):String{
             return text.lowercase()
         }
         fun uppercaseTransform(text: String): String {
-            return text.uppercase()
+                return text.uppercase()
         }
         fun reverseTransform(text: String): String {
             return text.reversed()
         }
 
         okButton.setOnClickListener {
-            if (lover.isChecked){
+            if (lower.isChecked){
                 val inputlower = setText.text.toString()
-                val resultlower=lowercaseTramsform(inputlower)
+                val resultlower=lowercaseTransform(inputlower)
                 setText.setText(resultlower)
                 result.text=resultlower
             }
@@ -54,7 +53,7 @@ class ReverseActivity : AppCompatActivity() {
             if (upper.isChecked){
                 val inputup = setText.text.toString()
                 val resultup = uppercaseTransform(inputup)
-                setText.setText(/* text = */ resultup)
+                setText.setText( resultup)
                 result.text = resultup
             }
         }
@@ -65,9 +64,17 @@ class ReverseActivity : AppCompatActivity() {
             result.text = ""
             upper.isChecked=false
             rev.isChecked=false
-            lover.isChecked=false
+            lower.isChecked=false
             setText.text.clear()
         }
+        lower.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { _, isChecked ->
+            upper.isEnabled = !isChecked
+        })
+        upper.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { _, isChecked ->
+            lower.isEnabled = !isChecked
+        })
+
+
 
 
         val buttonBacks = findViewById<Button>(R.id.buttons_back)
